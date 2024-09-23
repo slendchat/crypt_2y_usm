fc5c2eb8501ecda996594edaa2cfbdec048354f1
Artur Mamaliga <54188589+slendchat@users.noreply.github.com>
2024-09-23 18:03:15 +0300

    Update DES_crypt.c

--- DES/src/DES_crypt.c
+++ DES/src/DES_crypt.c
@@ -15,179 +15,18 @@ uint64_t join_half_keys(uint32_t keyleft, uint32_t keyright) ;
 void form_keys48b(uint32_t shifted_key_pairs[16][2], uint64_t *keys48b);
 void key_gen(uint64_t *keys48b, uint64_t key);
 
+uint64_t initial_permutation(uint64_t data_block);
+void div_block64_to_blocks32(uint32_t* left, uint32_t* right, uint64_t block64);
+void sblock_fill(uint8_t sblock6b[8], uint64_t block64b);
+uint8_t sblock_resolve(uint8_t sblock6b, int i);
+uint32_t join4b_to_32b(uint8_t sblock4b[8]);
+uint32_t permutation(uint32_t sblock_joined);
+uint64_t concat_blocks32_to64(uint32_t left_block32, uint32_t right_block32);
+uint64_t final_permutation(uint64_t block64);
+uint64_t encrypt_DES(uint64_t data_block, uint64_t keys48b[16]);
+uint64_t* reverse_array(const uint64_t arr[], int size);
+void printchars(uint64_t value);
 
-uint64_t initial_permutation(uint64_t data_block)
-{
-  uint64_t block_IP = 0;
-
-  for (int i = 0; i < 64; i++){
-    block_IP |= ((( data_block >> (64 - IP[i]) )&0x01) << (63 - i));
-  }
-  printf("[+] Initial permutation:\n");
-  print_binary64(block_IP);
-  return block_IP;
-}
-
-void div_block64_to_blocks32(uint32_t* left, uint32_t* right, uint64_t block64)
-{
-  *left = (uint32_t)(block64 >> 32);
-  *right = (uint32_t)(block64 & 0xFFFFFFFF);
-  printf("[+] left block half:\n");
-  print_binary32(*left);
-  printf("[+] right block half:\n");
-  print_binary32(*right);
-  return;
-}
-
-void sblock_fill(uint8_t sblock6b[8], uint64_t block64b)
-{
-  // for (int i = 8; i > 0; i--){
-  //   sblock6b[8-i] = ((uint8_t)((block64b >> (16 + 6*(i-1))) & 0b111111)<<2);
-  //   printf("[+] S block: %d\n",9-i);
-  //   print_binary8(sblock6b[8-i]);
-  // }
-  for (int i = 0; i < 8; i++) {
-    sblock6b[i] = ((uint8_t)((block64b >> (16 + 6*(7-i))) & 0b111111)<<2);
-    printf("[+] S block: %d\n", i+1);
-    print_binary8(sblock6b[i]);
-  }
-}
-
-uint8_t sblock_resolve(uint8_t sblock6b, int i)
-{
-  uint8_t raw = (((sblock6b & 0b10000000) >> 7)<<1) | ((sblock6b & 0b00000100)>>2);
-  printf("raw:%d\n",raw);
-  uint8_t col = (sblock6b & 0b01111000) >> 3;
-  printf("col:%d\n",col);
-  uint8_t res = S[i][raw][col];
-  printf("res:%d\n",res);
-  return res;
-}
-
-uint32_t join4b_to_32b(uint8_t sblock4b[8])
-{
-  uint32_t res = 0;
-  for (int i = 0; i < 8; i++){
-    res |= (((uint32_t)sblock4b[i])<<(4*(7-i)));
-  }
-  printf("[+] Sblock joined to 32b:\n");
-  print_binary32(res);
-  return res;
-  
-}
-
-uint32_t permutation(uint32_t sblock_joined)
-{
-  uint32_t res = 0;
-  for (int i = 0; i < 32; i++){
-    res |= ((sblock_joined >> (32-P[i])) & 1) << (31-i);
-  }
-  printf("[+] Block_p:\n");
-  print_binary32(res);
-  return res;
-}
-
-uint64_t concat_blocks32_to64(uint32_t left_block32, uint32_t right_block32)
-{
-  uint64_t res = 0;
-  res = (((uint64_t)right_block32)<<32)|((uint64_t)left_block32);
-  return res;
-}
-
-uint64_t final_permutation(uint64_t block64)
-{
-  uint64_t res = 0;
-  for (int i = 0; i < 64; i++){
-    res |= (((block64 >> (64 - PI[i]))) & 0x1)<<(63 - i);
-  }
-  return res;
-}
-
-uint64_t encrypt_DES(uint64_t data_block, uint64_t keys48b[16])
-{
-  uint32_t left_data = 0;
-  uint32_t right_data = 0;
-  uint32_t block_xored = 0;
-  uint64_t block_IP;
-  uint64_t block_concated = 0;
-  uint64_t block_crypted = 0;
-
-  block_IP = initial_permutation(data_block);
-  div_block64_to_blocks32(&left_data, &right_data, block_IP);
-  
-
-  for (int step = 0; step < 16; step++){
-    printf("[+]--------------[ STEP:| %d | ]--------------[+]\n",step+1);
-    uint64_t block_E = 0;
-    uint8_t sblock6b[8] = {0};
-    uint8_t sblock4b[8] = {0};
-    uint32_t sblock_joined = 0;
-    uint32_t block_P = 0;
-
-    for (int i = 0; i < 48; i++){
-      block_E |= (((uint64_t)(right_data >> (32 - E[i]))) & 0x1)<<(63 - i);
-    }
-    printf("[+] Block_e:\n");
-    print_binary64(block_E);
-
-    block_E ^= keys48b[step];
-
-    printf("[+] block xored with key:\n");
-    print_binary64(block_E);
-
-    sblock_fill(sblock6b,block_E);
-    for (int i = 0; i < 8; i++){
-      sblock4b[i] = sblock_resolve(sblock6b[i],i);
-      printf("[+] Sblock 4b %d:\n",i);
-      print_binary8(sblock4b[i]);
-    }
-
-    sblock_joined = join4b_to_32b(sblock4b);
-    block_P = permutation(sblock_joined);
-
-    left_data = left_data ^ block_P;
-    printf("[+] Left block after XOR:\n");
-    print_binary32(left_data);
-
-
-    //SWAP left and right
-    uint32_t temp = left_data;
-    left_data = right_data;
-    right_data = temp;
-  }
-
-  block_concated = concat_blocks32_to64(left_data,right_data);
-  printf("[+] Concated block:\n");
-  print_binary64(block_concated);
-
-  block_crypted = final_permutation(block_concated);
-  printf("[+] Crypted block:\n");
-  print_binary64(block_crypted);
-  
-  return block_crypted;
-}
-
-uint64_t* reverse_array(const uint64_t arr[], int size)
-{
-    uint64_t* reversed = (uint64_t*) malloc(size * sizeof(uint64_t));  // Dynamically allocate a new array for the reversed result
-    memset(reversed, 0, size * sizeof(uint64_t));
-    if (reversed == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
-    for (int i = 0; i < size; ++i) {
-        reversed[i] = arr[size - 1 - i];  // Copy elements from the end of the original array to the new array
-    }
-    return reversed;  // Return the pointer to the new reversed array
-}
-
-void printchars(uint64_t value) {
-    // Iterate through each of the 8 bytes (characters) starting from the most significant byte
-    for (int i = 7; i >= 0; --i) {
-        char ch = (char)((value >> (i * 8)) & 0xFF);  // Extract each byte
-        printf("%c", ch);  // Print the character
-    }
-}
 
 int main(void)
 {
@@ -396,5 +235,180 @@ void key_gen(uint64_t *keys48b, uint64_t key)
 
 }
 
+
+
+uint64_t initial_permutation(uint64_t data_block)
+{
+  uint64_t block_IP = 0;
+
+  for (int i = 0; i < 64; i++){
+    block_IP |= ((( data_block >> (64 - IP[i]) )&0x01) << (63 - i));
+  }
+  printf("[+] Initial permutation:\n");
+  print_binary64(block_IP);
+  return block_IP;
+}
+
+void div_block64_to_blocks32(uint32_t* left, uint32_t* right, uint64_t block64)
+{
+  *left = (uint32_t)(block64 >> 32);
+  *right = (uint32_t)(block64 & 0xFFFFFFFF);
+  printf("[+] left block half:\n");
+  print_binary32(*left);
+  printf("[+] right block half:\n");
+  print_binary32(*right);
+  return;
+}
+
+void sblock_fill(uint8_t sblock6b[8], uint64_t block64b)
+{
+  // for (int i = 8; i > 0; i--){
+  //   sblock6b[8-i] = ((uint8_t)((block64b >> (16 + 6*(i-1))) & 0b111111)<<2);
+  //   printf("[+] S block: %d\n",9-i);
+  //   print_binary8(sblock6b[8-i]);
+  // }
+  for (int i = 0; i < 8; i++) {
+    sblock6b[i] = ((uint8_t)((block64b >> (16 + 6*(7-i))) & 0b111111)<<2);
+    printf("[+] S block: %d\n", i+1);
+    print_binary8(sblock6b[i]);
+  }
+}
+
+uint8_t sblock_resolve(uint8_t sblock6b, int i)
+{
+  uint8_t raw = (((sblock6b & 0b10000000) >> 7)<<1) | ((sblock6b & 0b00000100)>>2);
+  printf("raw:%d\n",raw);
+  uint8_t col = (sblock6b & 0b01111000) >> 3;
+  printf("col:%d\n",col);
+  uint8_t res = S[i][raw][col];
+  printf("res:%d\n",res);
+  return res;
+}
+
+uint32_t join4b_to_32b(uint8_t sblock4b[8])
+{
+  uint32_t res = 0;
+  for (int i = 0; i < 8; i++){
+    res |= (((uint32_t)sblock4b[i])<<(4*(7-i)));
+  }
+  printf("[+] Sblock joined to 32b:\n");
+  print_binary32(res);
+  return res;
+  
+}
+
+uint32_t permutation(uint32_t sblock_joined)
+{
+  uint32_t res = 0;
+  for (int i = 0; i < 32; i++){
+    res |= ((sblock_joined >> (32-P[i])) & 1) << (31-i);
+  }
+  printf("[+] Block_p:\n");
+  print_binary32(res);
+  return res;
+}
+
+uint64_t concat_blocks32_to64(uint32_t left_block32, uint32_t right_block32)
+{
+  uint64_t res = 0;
+  res = (((uint64_t)right_block32)<<32)|((uint64_t)left_block32);
+  return res;
+}
+
+uint64_t final_permutation(uint64_t block64)
+{
+  uint64_t res = 0;
+  for (int i = 0; i < 64; i++){
+    res |= (((block64 >> (64 - PI[i]))) & 0x1)<<(63 - i);
+  }
+  return res;
+}
+
+uint64_t encrypt_DES(uint64_t data_block, uint64_t keys48b[16])
+{
+  uint32_t left_data = 0;
+  uint32_t right_data = 0;
+  uint32_t block_xored = 0;
+  uint64_t block_IP;
+  uint64_t block_concated = 0;
+  uint64_t block_crypted = 0;
+
+  block_IP = initial_permutation(data_block);
+  div_block64_to_blocks32(&left_data, &right_data, block_IP);
+  
+
+  for (int step = 0; step < 16; step++){
+    printf("[+]--------------[ STEP:| %d | ]--------------[+]\n",step+1);
+    uint64_t block_E = 0;
+    uint8_t sblock6b[8] = {0};
+    uint8_t sblock4b[8] = {0};
+    uint32_t sblock_joined = 0;
+    uint32_t block_P = 0;
+
+    for (int i = 0; i < 48; i++){
+      block_E |= (((uint64_t)(right_data >> (32 - E[i]))) & 0x1)<<(63 - i);
+    }
+    printf("[+] Block_e:\n");
+    print_binary64(block_E);
+
+    block_E ^= keys48b[step];
+
+    printf("[+] block xored with key:\n");
+    print_binary64(block_E);
+
+    sblock_fill(sblock6b,block_E);
+    for (int i = 0; i < 8; i++){
+      sblock4b[i] = sblock_resolve(sblock6b[i],i);
+      printf("[+] Sblock 4b %d:\n",i);
+      print_binary8(sblock4b[i]);
+    }
+
+    sblock_joined = join4b_to_32b(sblock4b);
+    block_P = permutation(sblock_joined);
+
+    left_data = left_data ^ block_P;
+    printf("[+] Left block after XOR:\n");
+    print_binary32(left_data);
+
+
+    //SWAP left and right
+    uint32_t temp = left_data;
+    left_data = right_data;
+    right_data = temp;
+  }
+
+  block_concated = concat_blocks32_to64(left_data,right_data);
+  printf("[+] Concated block:\n");
+  print_binary64(block_concated);
+
+  block_crypted = final_permutation(block_concated);
+  printf("[+] Crypted block:\n");
+  print_binary64(block_crypted);
+  
+  return block_crypted;
+}
+
+uint64_t* reverse_array(const uint64_t arr[], int size)
+{
+    uint64_t* reversed = (uint64_t*) malloc(size * sizeof(uint64_t));  // Dynamically allocate a new array for the reversed result
+    memset(reversed, 0, size * sizeof(uint64_t));
+    if (reversed == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    for (int i = 0; i < size; ++i) {
+        reversed[i] = arr[size - 1 - i];  // Copy elements from the end of the original array to the new array
+    }
+    return reversed;  // Return the pointer to the new reversed array
+}
+
+void printchars(uint64_t value) 
+{
+    // Iterate through each of the 8 bytes (characters) starting from the most significant byte
+    for (int i = 7; i >= 0; --i) {
+        char ch = (char)((value >> (i * 8)) & 0xFF);  // Extract each byte
+        printf("%c", ch);  // Print the character
+    }
+}
 
 
